b24766410e62ed9305b01202e87de11303593d8b
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-06-16 14:11:20 +0800

    feat(footer): add LinkedIn link to Footer
    Added a link to LinkedIn in the Footer component and updated the CSS module for styling.
    
    - Updated Footer component to include a LinkedIn link with target='_blank' and rel='noopener noreferrer'
    - Modified Footer.module.css to style the new link
    
    This commit enhances the Footer component by adding a LinkedIn link and ensuring proper styling.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,17 @@
+import css from './Footer.module.css';
+
 export const Footer = () => {
-  return <footer>&copy; {new Date().getFullYear()} goit-react-hw-08-phonebook</footer>;
+  return (
+    <footer className={css.footerContainer}>
+      &copy; {new Date().getFullYear()}{' '}
+      <a
+        className={css.footerLink}
+        href='https://www.linkedin.com/in/marventures/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Marvin Pacis
+      </a>
+    </footer>
+  );
 };
