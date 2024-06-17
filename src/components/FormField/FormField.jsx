9384af0db27674227383221cd9578b7ef9496049
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-06-17 16:39:15 +0800

    feat(input) implement InputMask for phone number
    - Added InputMask component to handle phone number input with formatting.
    - Updated FormField component to conditionally render InputMask based on the 'phone' type.

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -1,20 +1,35 @@
 import css from './FormField.module.css';
 import PropTypes from 'prop-types';
+import InputMask from 'react-input-mask';
 
 export const FormField = ({ label, name, type, formik, icon: Icon }) => {
+  const isError = formik.touched[name] && formik.errors[name];
+
   return (
     <label className={css.formField}>
       <p className={css.formLabel}>{label}</p>
-      <input
-        className={`${css.formInput} ${
-          formik.touched[name] && formik.errors[name] && css.errorInput
-        }`}
-        type={type}
-        name={name}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values[name]}
-      />
+      {type === 'phone' ? (
+        <InputMask
+          mask='+99 (999) 999 9999'
+          maskChar=''
+          className={`${css.formInput} ${isError && css.errorInput}`}
+          type='text'
+          name={name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values[name]}
+          placeholder='+63 (912) 345 6789'
+        />
+      ) : (
+        <input
+          className={`${css.formInput} ${isError && css.errorInput}`}
+          type={type}
+          name={name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values[name]}
+        />
+      )}
       <Icon className={css.formIcon} />
       {formik.touched[name] && formik.errors[name] && (
         <div className={css.error}>{formik.errors[name]}</div>
