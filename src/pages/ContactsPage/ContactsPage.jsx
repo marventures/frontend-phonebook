bf45bc132fda871410adbc5429671defd15d66e7
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-06-19 18:49:50 +0800

    feat: add welcome message on contacts page
    - Added a welcome message with the user's first name on the contacts page.
    - Updated the UserMenu component to display the user's full name (firstName and lastName) instead of just the first name.
    - Ensured consistent user information display across the application.
    
    This feature enhances the user interface by providing a personalized welcome message on the contacts page and detailed user information in the UserMenu.

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,13 +1,17 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Filter } from '../../components/Filter/Filter';
 import { ContactList } from '../../components/ContactList/ContactList';
+import { useAuth } from '../../hooks/useAuth';
 
 const ContactsPage = () => {
+  const { user } = useAuth();
   return (
     <HelmetProvider>
       <Helmet>
         <title>Phonebook</title>
       </Helmet>
+      <h2>{`Welcome ${user.firstName}`}!</h2>
+      <br />
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
